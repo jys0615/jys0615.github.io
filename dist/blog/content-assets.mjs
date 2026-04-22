cfbf7c3e03b839fab4b170f7c07feeeab0bdf917
jys0615 <yoonsuh0615@khu.ac.kr>
2026-04-22 10:08:35 +0900

    test: add blog post and Front Matter CMS config

--- dist/blog/content-assets.mjs
+++ dist/blog/content-assets.mjs
@@ -1,5 +1,1 @@
-
-import __ASTRO_IMAGE_IMPORT_Z9rgle from "@/assets/images/forrest-gump-quote.png?astroContentImageFlag=&importer=src%2Fdata%2Fblog%2Fhow-to-update-dependencies.md";
-import __ASTRO_IMAGE_IMPORT_ZNsEp4 from "../../assets/images/forrest-gump-quote.png?astroContentImageFlag=&importer=src%2Fdata%2Fblog%2Fhow-to-update-dependencies.md";
-export default new Map([["@/assets/images/forrest-gump-quote.png?astroContentImageFlag=&importer=src%2Fdata%2Fblog%2Fhow-to-update-dependencies.md", __ASTRO_IMAGE_IMPORT_Z9rgle], ["../../assets/images/forrest-gump-quote.png?astroContentImageFlag=&importer=src%2Fdata%2Fblog%2Fhow-to-update-dependencies.md", __ASTRO_IMAGE_IMPORT_ZNsEp4]]);
-		
+export default new Map();
